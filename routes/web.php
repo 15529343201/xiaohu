ca733baac1f2012b9d72f1717d479f4b1b962606
15529343201 <15529343201@139.com>
2017-12-31 10:48:28 +0800

    add 提交路由

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+function rq($key=null,$default=null){
+    if(!$key)
+      return Request::all();
+    return Request::get($key,$default);
+}
 function user_ins(){
     return new App\User;
+}
+function question_ins(){
+    return new App\Question;
+}
+function answer_ins(){
+    return new App\Answer;
+}
+function comment_ins(){
+    return new App\Comment;
 }
 Route::any('api',function(){
     return ['version' => 0.1];
@@ -25,6 +39,37 @@ Route::any('api/login',function(){
 });
 Route::any('api/logout',function(){
     return user_ins()->logout();
+});
+/*增加问题API的建立*/
+Route::any('api/question/add',function(){
+    return question_ins()->add();
+});
+Route::any('api/question/change',function(){
+    return question_ins()->change();
+});
+Route::any('api/question/read',function(){
+    return question_ins()->read();
+});
+Route::any('api/question/remove',function(){
+    return question_ins()->remove();
+});
+Route::any('api/answer/add',function(){
+    return answer_ins()->add();
+});
+Route::any('api/answer/change',function(){
+    return answer_ins()->change();
+});
+Route::any('api/answer/read',function(){
+    return answer_ins()->read();
+});
+Route::any('api/comment/add',function(){
+    return comment_ins()->add();
+});
+Route::any('api/comment/read',function(){
+    return comment_ins()->read();
+});
+Route::any('api/comment/remove',function(){
+    return comment_ins()->remove();
 });
 Route::any('test',function(){
     dd(user_ins()->is_logged_in());
