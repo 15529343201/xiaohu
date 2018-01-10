826fadc6e9aeb06d0e1883d241097cbd647639d2
15529343201 <15529343201@139.com>
2018-01-11 00:30:33 +0800

    add index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -44,8 +44,44 @@
 </body>
 
 <script type="text/ng-template" id="home.tpl">
-    <div class="home container">
-      首页
+    <div ng-controller="HomeController" class="home card container">
+      <h1>最近动态</h1>
+      <div class="hr"></div>
+      <div class="item-set">
+        <div ng-repeat="item in Timeline.data" class="item">
+          <div class="vote"></div>
+          <div class="feed-item-content">
+            <div ng-if="item.question_id" class="content-act">[: item.user.username :]添加了回答</div>
+            <div ng-if="!item.question_id" class="content-act">[: item.user.username :]添加了提问</div>
+            <div class="title">[: item.title :]</div>
+            <div class="content-owner">[: item.user.username :]
+              <span class="desc">12323232323233</span>
+            </div>
+            <div class="content-main">
+              [: item.desc :]
+            </div>
+            <div class="action-set">
+              <div class="comment">评论</div>
+            </div>
+
+            <div class="comment-block">
+              <div class="hr"></div>
+              <div class="comment-item-set">
+                <div class="rect"></div>
+                <div class="comment-item clearfix">
+                  <div class="user">李明</div>
+                  <div class="comment-content">
+                    11111111111111111111111
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="hr"></div>
+        </div>
+        <div ng=if="Timeline.pending" class="tac">加载中...</div>
+        <div ng-if="Timeline.no_more_data" class="tac">没有更多数据啦</div>
+      </div>
     </div>
 </script>
 
